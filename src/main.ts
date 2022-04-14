146e5062e0c1487326713fb81b0df1bcd2064eeb
Juscilan Moreto <juscilan.moreto@vortigo.com.br>
2022-04-14 19:49:04 -0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
   const host = process.env.HOST || '0.0.0.0';
-  await app.listen(port, host);
+  await app.listen(Number(port), host);
 }
 bootstrap();
