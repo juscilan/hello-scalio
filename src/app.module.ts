a0856648b5a080a73e84d32b9bf893e1e2ce03a9
Juscilan Moreto <juscilan.moreto@vortigo.com.br>
2022-04-14 19:53:00 -0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
-  imports: [
-    PostsModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-    }),
-  ],
+  imports: [PostsModule, ,],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+// ServeStaticModule.forRoot({
+//   rootPath: join(__dirname, '..', 'client'),
+// })
